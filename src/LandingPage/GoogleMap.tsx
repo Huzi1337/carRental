257fcf9d649f07b8ee4c4b17a476ff64b87e03e6
Krzysztof Jagoda <krzysztof.jagoda99@gmail.com>
2024-02-28 11:29:29 +0100

    Map fix v3

--- src/LandingPage/GoogleMap.tsx
+++ src/LandingPage/GoogleMap.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import "./GoogleMap.scss";
 
@@ -82,7 +82,7 @@ const GoogleMap: React.FC = () => {
         });
       }
     }
-  }, []);
+  }, [mapLoaded]);
 
   return (
     <div
