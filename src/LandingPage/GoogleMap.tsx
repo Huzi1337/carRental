855385953051857c7529f6a4d5768bd57747ed2f
Krzysztof Jagoda <krzysztof.jagoda99@gmail.com>
2024-02-28 11:12:46 +0100

    Updated deprecated google map rendering method

--- src/LandingPage/GoogleMap.tsx
+++ src/LandingPage/GoogleMap.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 
 import "./GoogleMap.scss";
 
@@ -19,73 +19,44 @@ const cities: City[] = [
 
 const GoogleMap: React.FC = () => {
   const mapRef = useRef<HTMLDivElement | null>(null);
-  const markersRef = useRef<google.maps.Marker[]>([]);
-  const mapInstanceRef = useRef<google.maps.Map | null>(null);
-  const [mapLoaded, setMapLoaded] = useState(false);
 
   useEffect(() => {
-    const loadMapScript = () => {
-      const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBoM2bSm8V4Fj1Mj2YSradgmcPIL7jUD6U&callback=initMap`;
-      script.defer = true;
-      script.async = true;
-      script.addEventListener("load", handleScriptLoad);
-      document.body.appendChild(script);
-    };
+    initMap();
 
-    const handleScriptLoad = () => {
-      setMapLoaded(true);
-    };
+    async function initMap() {
+      const { current } = mapRef;
+      if (current) {
+        const { Map } = (await google.maps.importLibrary(
+          "maps"
+        )) as google.maps.MapsLibrary;
+        const { AdvancedMarkerElement, PinElement } =
+          (await google.maps.importLibrary(
+            "marker"
+          )) as google.maps.MarkerLibrary;
 
-    if (!window.google || !window.google.maps) {
-      (window as any).initMap = handleScriptLoad;
-      loadMapScript();
-    } else {
-      setMapLoaded(true);
+        const map = new Map(current as HTMLElement, {
+          center: { lat: 52.4194, lng: 19.1451 },
+          zoom: 6,
+          disableDefaultUI: true,
+          mapId: "4504f8b37365c3d0",
+        });
+
+        cities.forEach((city) => {
+          const { lat, lng } = city;
+          const pinOptions = new PinElement({
+            background: "#2A6FDB",
+            glyphColor: "#FFFFFF",
+            borderColor: "#2A6FDB",
+          });
+          new AdvancedMarkerElement({
+            map,
+            position: { lat, lng },
+            content: pinOptions.element,
+          });
+        });
+      }
     }
   }, []);
-
-  useEffect(() => {
-    if (mapLoaded) {
-      const mapOptions: google.maps.MapOptions = {
-        center: { lat: 51.9194, lng: 19.1451 },
-        zoom: 6,
-        disableDefaultUI: true,
-      };
-
-      const map = new google.maps.Map(mapRef.current!, mapOptions);
-      mapInstanceRef.current = map;
-
-      const markers = cities.map((city) => {
-        const markerIcon = {
-          url: "https://maps.google.com/mapfiles/ms/icons/blue.png",
-          scaledSize: new google.maps.Size(32, 32),
-          fillColor: "blue",
-        };
-
-        const marker = new google.maps.Marker({
-          position: { lat: city.lat, lng: city.lng },
-          title: city.name,
-          map: map,
-          icon: markerIcon,
-        });
-        return marker;
-      });
-
-      markersRef.current = markers;
-
-      return () => {
-        markersRef.current.forEach((marker) => {
-          marker.setMap(null);
-        });
-        markersRef.current = [];
-
-        if (mapInstanceRef.current) {
-          mapInstanceRef.current = null;
-        }
-      };
-    }
-  }, [mapLoaded]);
 
   return (
     <div
